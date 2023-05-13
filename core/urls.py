c49dabd2629fa6f07380622a86756c8c252bfc11
Filip <filipelblag@gmail.com>
2023-05-13 15:45:47 +0200

    implemented redirect

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from .views import RedirectToShortenedView
+
 urlpatterns = [
     path("api/", include("core.api.urls")),
+    path("<short>/", RedirectToShortenedView.as_view(), name="redirect-to-shortened"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from django.views.generic import RedirectView
+
+from .models import Link
+
+
+class RedirectToShortenedView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        short_url = self.request.build_absolute_uri()[:-1]  # remove the trailing "/"
+        link = get_object_or_404(Link, short_url=short_url)
+
+        return link.long_url
